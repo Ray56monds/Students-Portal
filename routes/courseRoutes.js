fa80201c7cbc8a7c0935f138ae1992ed3fd2948a
Ray56monds <magezir2@stanbic.com>
2024-03-15 20:05:44 +0300

    commit changes in the courseRoutes.js file

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -6,7 +6,7 @@ import * as courseController from '../controllers/courseController.js'; // Impor
 const router = express.Router();
 
 // Define routes
-router.post('/', courseController.createCourse); // Create a new course
+router.post('/', courseController.createCourse);// Create a new course
 router.get('/', courseController.getAllCourses); // Get all courses
 router.get('/:courseId', courseController.getCourseById); // Get a course by ID
 router.patch('/:courseId', courseController.updateCourseById); // Update a course by ID
